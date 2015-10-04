a65baebfa8fd9ed76effaa121402d7fb96d054f6
galinka-zch <galinka-zch@mail.ru>
2015-10-04 23:21:18 +0300

    'Cycle_Print'

--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+
 public class test {
     public static void main(String args[]) {
         System.out.println("Initial data:");
@@ -24,16 +25,10 @@ public class test {
                 break;
             }
         }
+
         System.out.println("After sorting:");
-        System.out.print(mas[0]+ "; ");
-        System.out.print(mas[1]+ "; ");
-        System.out.print(mas[2]+ "; ");
-        System.out.print(mas[3]+ "; ");
-        System.out.print(mas[4]+ "; ");
-        System.out.print(mas[5]+ "; ");
-        System.out.print(mas[6]+ "; ");
-        System.out.print(mas[7]+ "; ");
-        System.out.print(mas[8]+ "; ");
-        System.out.print(mas[9]+ ". ");
+        for (int i=0; i<mas.length; i++ )
+        System.out.print(mas[i]+ "; ");
+
     }
 }
