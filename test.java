fdd5f5d178865ad10b85f925ed34204243e88e2b
galinka-zch <galinka-zch@mail.ru>
2015-10-05 00:12:53 +0300

    'added_spaces'

--- test.java
+++ test.java
@@ -2,26 +2,26 @@
 public class test {
     public static void main(String args[]) {
         System.out.println("Initial data:");
-        int[] mas = {33,27,9,12,14,27,64,42,15,58};
+        int[] mas = {33, 27, 9, 12, 14, 27, 64, 42, 15, 58};
 
-        for(int i=0; i<mas.length; i++)
+        for (int i = 0; i < mas.length; i++)
             System.out.print(mas[i] + "; ");
 
 
-        for (int i=0; i<=mas.length; i++){
-            int j=mas.length-1;
+        for (int i = 0; i <= mas.length; i++){
+            int j = mas.length-1;
             boolean control_sorting = true;
 
             while (j > i){
-                if (mas[j-1]>mas[j]){
+                if (mas[j-1] > mas[j]){
                     int a = mas[j-1];
-                    mas[j-1]=mas[j];
-                    mas[j]=a;
+                    mas[j-1] = mas[j];
+                    mas[j] = a;
                     control_sorting = false;
                 }
                 j--;
             }
-            if(true==control_sorting){
+            if (control_sorting){
                 break;
             }
         }
