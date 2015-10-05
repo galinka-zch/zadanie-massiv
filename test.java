56076f2217541a46dcef50a69642fd0f4ff7e0a8
galinka-zch <galinka-zch@mail.ru>
2015-10-06 00:37:09 +0300

    '#4_Исправить_алгоритм_на_строки'

--- test.java
+++ test.java
@@ -2,26 +2,31 @@
 public class test {
     public static void main(String args[]) {
         System.out.println("Initial data:");
-        int[] mas = {33, 27, 9, 12, 14, 27, 64, 42, 15, 58};
+        String mas[] = {"for", "never", "was", "a",
+                        "store", "of", "more", "woe",
+                        "than", "this", "of", "juliet",
+                        "and", "her", "romeo"};
 
         for (int i = 0; i < mas.length; i++)
             System.out.print(mas[i] + "; ");
 
-
         for (int i = 0; i <= mas.length; i++){
             int j = mas.length-1;
-            boolean control_sorting = true;
+            boolean controlSorting = true;
 
             while (j > i){
-                if (mas[j-1] > mas[j]){
-                    int a = mas[j-1];
+                if (mas[j-1].compareTo(mas[j]) > 0){
+                    String a = mas[j-1];
                     mas[j-1] = mas[j];
                     mas[j] = a;
-                    control_sorting = false;
+                    controlSorting = false;
                 }
-                j--;
+
+            j--;
+
             }
-            if (control_sorting){
+
+            if (controlSorting){
                 break;
             }
         }
