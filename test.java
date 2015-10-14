ee77c9d022a3a1d803457038a2d48a9f7b18d462
galinka-zch <galinka_zch@mail.ru>
2015-10-14 19:24:40 +0300

    '#3_Java_Code_Convention'

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 
-public class BubleSort {
+public class BubbleSort {
     public static void main(String args[]) {
         System.out.println("Initial data:");
         String mas[] = {"for", "never", "was", "a",
@@ -8,19 +8,19 @@ public class BubleSort {
                         "and", "her", "romeo"};
         for (int i = 0; i < mas.length; i++)
             System.out.print(mas[i] + "; ");
-        for (int i = 0; i <= mas.length; i++){
+        for (int i = 0; i <= mas.length; i++) {
             int j = mas.length-1;
             boolean controlSorting = true;
-            	while (j > i){
-                	if (mas[j-1].compareTo(mas[j]) > 0){
+            	while (j > i) {
+                	if (mas[j-1].compareTo(mas[j]) > 0) {
                     		String a = mas[j-1];
                     		mas[j-1] = mas[j];
                     		mas[j] = a;
                     		controlSorting = false;
-                		}
+                	}
             		j--;
             	}
-            if (controlSorting){
+            if (controlSorting) {
                 break;
             }
         }
