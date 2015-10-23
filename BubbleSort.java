e68199a98534f2f4d64d24b4b3bd612801e7e640
galinka-zch <galinka_zch@mail.ru>
2015-10-23 14:16:53 +0300

    Java Code Convention #3

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,31 +3,31 @@ public class BubbleSort {
     public static void main(String args[]) {
         System.out.println("Initial data:");
         String mas[] = {"for", "never", "was", "a",
-                        "store", "of", "more", "woe",
-                        "than", "this", "of", "juliet",
-                        "and", "her", "romeo"};
+                "store", "of", "more", "woe",
+                "than", "this", "of", "juliet",
+                "and", "her", "romeo"};
         for (int i = 0; i < mas.length; i++) {
             System.out.print(mas[i] + "; ");
-	}
+        }
         for (int i = 0; i <= mas.length; i++) {
-            int j = mas.length-1;
+            int j = mas.length - 1;
             boolean controlSorting = true;
             while (j > i) {
-                if (mas[j-1].compareTo(mas[j]) > 0) {
-                    String a = mas[j-1];
-                    mas[j-1] = mas[j];
+                if (mas[j - 1].compareTo(mas[j]) > 0) {
+                    String a = mas[j - 1];
+                    mas[j - 1] = mas[j];
                     mas[j] = a;
                     controlSorting = false;
                 }
-            	j--;
+                j--;
             }
             if (controlSorting) {
                 break;
             }
         }
         System.out.println("After sorting:");
-        for (int i = 0; i < mas.length; i++ ) {
+        for (int i = 0; i < mas.length; i++) {
             System.out.print(mas[i] + "; ");
-	}
+        }
     }
 }
